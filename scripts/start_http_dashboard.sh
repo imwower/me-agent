b28f8084c601ebaeb01b2c3dc26daec6f8a24294
jiaozhiwang <george674834080@gmail.com>
2025-11-24 18:53:40 +0800

    chore: 修正 dashboard 脚本的退出方式

--- scripts/start_http_dashboard.sh
+++ scripts/start_http_dashboard.sh
@@ -4,10 +4,17 @@ set -e
 cd "$(dirname "$0")/.."
 
 python - <<'PY'
+import sys
+import time
 from me_core.world_model import SimpleWorldModel
 from me_core.self_model import SimpleSelfModel
 from me_ext.http_api import serve_http
 
 serve_http(SimpleWorldModel(), SimpleSelfModel(), port=8000)
-input("HTTP server on 8000, Enter to stop")
+print("HTTP server on 8000. 按 Ctrl+C 停止，或在非交互模式下直接 kill 进程。")
+try:
+    while True:
+        time.sleep(1)
+except KeyboardInterrupt:
+    sys.exit(0)
 PY
